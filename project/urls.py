e884b759b8c93221ea9b496c9f4d73e5d74570d9
Pedro Regis <pedroalencarregis@hotmail.com>
2016-10-22 16:35:25 -0300

    Varias modificações

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^results_bhaskara/', views.results_bhaskara, name='results_bhaskara'),
     url(r'^inputs_bhaskara', views.inputs_bhaskara, name="inputs_bhaskara"),
+    url(r'^results_bhaskara/', views.results_bhaskara, name='results_bhaskara'),    
     url(r'^inputs_calha_parshall', views.inputs_calha_parshall, name="inputs_calha_parshall"),
+    url(r'^results_calha_parshall/', views.results_calha_parshall, name='results_calha_parshall'),
 ]
--- project/views.py
+++ project/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 
 from project.bhaskara.bhaskara import Bhaskara
 
+#from project.bhaskara.bhaskara import Bhaskara
+
 # Create your views here.
 
 
 
 def index(request):
     return render(request, "project/home.html")
+
 
 def inputs_bhaskara(request):     
     return render(request,'project/bhaskara/inputs_bhaskara.html')
@@ -27,34 +30,32 @@ def results_bhaskara(request):
         dic={"a":a, "b":b, "c":c, "r1":r1, "r2":r2}
         
     else:
-        inputs_bhaskara(request)
-        
+        inputs_bhaskara(request)        
 #    return render(request,'personal/resultado.html', dic)
-
     return render(request,'project/bhaskara/results_bhaskara.html', dic)
+
+
+#   Coisas da Calha Parshall
 
 
 def inputs_calha_parshall(request):     
     return render(request,'project/calha_parshall/inputs_calha_parshall.html')
 
 
-def calha_parshall(request):
-    if request.method == "POST":
-        a=int(request.POST["a"])
-        b=int(request.POST["b"])
-        c=int(request.POST["c"])
+def results_calha_parshall(request):
 
-        eq_bhaskara=Bhaskara(a,b,c)
-        eq_bhaskara.roots()
-        r1=eq_bhaskara.r1
-        r2=eq_bhaskara.r2
+#    if request.method == "POST":
+#        Q=int(request.POST["Q"])
+#        g=int(request.POST["g"])
+#        T=int(request.POST["T"])
 
-        dic={"a":a, "b":b, "c":c, "r1":r1, "r2":r2}
+#        cp=Bhaskara(a,b,c)
+#        cp.dimensiona()
+
+#        dic={"a":a, "b":b, "c":c, "r1":r1, "r2":r2}
         
-    else:
-        inputs_bhaskara(request)
-        
-#    return render(request,'personal/resultado.html', dic)
-
-    return render(request,'project/calha_parshall/results_calha_parshall.html', dic)    
+#    else:
+#        inputs_calha_parshall(request)
+#    return render(request,'project/calha_parshall/results_calha_parshall.html', dic)    
+    return render(request,'project/calha_parshall/results_calha_parshall.html')    
 
