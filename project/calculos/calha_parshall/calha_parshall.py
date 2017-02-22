455833d227d3c1b3559210edfeaf50412b6ac424
Pedro Regis <pedroalencarregis@hotmail.com>
2017-02-22 20:30:01 -0300

    Modificação para atender a PEP8

--- project/calculos/calha_parshall/calha_parshall.py
+++ project/calculos/calha_parshall/calha_parshall.py
@@ -93,24 +93,24 @@ class DensidadeViscosidade():
     __slots__=()
 
     def frho(self, T):
-        t0=3.9818
-        A=7.0134*10**(-8)
-        B=7.926504*10**(-6)
-        C=-7.575677*10**(-8)
-        D=7.314894*10**(-10)
-        E=-3.596458*10**(-12)
-        rho=999.97358
-        L=[A, B, C, D, E]
-        soma=0
+        t0 = 3.9818
+        A = 7.0134*10**(-8)
+        B = 7.926504*10**(-6)
+        C = -7.575677*10**(-8)
+        D = 7.314894*10**(-10)
+        E = -3.596458*10**(-12)
+        rho = 999.97358
+        L = [A, B, C, D, E]
+        soma = 0
         for i in range(len(L)):
-            soma+=L[i]*(T-t0)**(i+1)
+            soma += L[i]*(T - t0)**(i + 1)
 
-        return rho*(1-soma)
+        return rho*(1 - soma)
 
     def fmu(self, T):
-        a=2.414*10**(-5)
-        b=247.8
-        c=140 - 273.15 # Conversão para graus Celsius
+        a = 2.414*10**(-5)
+        b = 247.8
+        c = 140 - 273.15 # Conversão para graus Celsius
         return a*10**(b/(T - c)) 
 
 
