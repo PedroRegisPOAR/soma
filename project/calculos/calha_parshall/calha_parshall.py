52e7fe6ad5ed90e47c654dbcd62d7c861090b43e
Pedro Regis <pedroalencarregis@hotmail.com>
2017-03-06 14:21:50 -0300

    Primeira versão da classe GerarPDF, e pequenas mudanças, como de L para Lr para representar o comprimento do ressalto.

--- project/calculos/calha_parshall/calha_parshall.py
+++ project/calculos/calha_parshall/calha_parshall.py
@@ -1,5 +1,17 @@
 ﻿import math
 
+#import jinja2
+#from jinja2 import Template
+
+import os
+import tempfile
+import subprocess
+from subprocess import Popen, PIPE
+from django.template import Context
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+from django.template.loader import render_to_string
 
 global cpinit
 
@@ -7,16 +19,23 @@ cpinit = {
     'Q':None,
     'g':None,
     'T':None,
+    'c':None,
     'iW':None,
 }
 
 global cpresults
 cpresults = {
-    'W':None,
     'mu':None,
     'rho':None,
+    'W':None,
+    'A':None,
+    'B':None,
+    'B23':None,
     'C':None,
     'D':None,
+    'E':None,
+    'F':None,
+    'L':None,
     'K':None,
     'N':None,
     'k':None,
@@ -32,7 +51,7 @@ cpresults = {
     'h2':None,
     'h3':None,
     'U3':None,
-    'L':None,
+    'Lr':None,
     'h':None,
     'Tm':None,
     'Gm':None,
@@ -72,22 +91,21 @@ class TabelasCalhaParshall():
                     [2440, 0.324, 0.623]]
 
 """
-
- for linha in t:
+for linha in t:
     for e in linha:
-        print('<td> ', e, ' </td>')
+    print('<td> ', e, ' </td>')
     print(" ")
 
-[[229, 880, 864, 380, 575, 763, 305, 457, 76, 114, 229, 1.486, 0.633],
-[305, 1372, 1344, 610, 845, 915, 610, 915, 76, 229, 305, 1.276, 0.657],
-[457, 1449, 1420, 762, 1026, 915, 610, 915, 76, 229, 457, 0.966, 0.65],
-[610, 1525, 1496, 915, 1207, 915, 610, 915, 76, 229, 610, 0.795, 0.64],
-[915, 1677, 1645, 1220, 1572, 915, 610, 915, 76, 229, 915, 0.608, 0.639],
-[1220, 1830, 1795, 1525, 1938, 915, 610, 915, 76, 229, 1220, 0.505, 0.634],
-[1525, 1983, 1941, 1830, 2303, 915, 610, 915, 76, 229, 1525, 0.436, 0.63],
-[1830, 2135, 2090, 2135, 2667, 915, 610, 915, 76, 229, 1830, 0.389, 0.627],
-[2135, 2288, 2240, 2440, 3030, 915, 610, 915, 76, 229, 2135, 0.355, 0.625],
-[2440, 2440, 2392, 2745, 3400, 915, 610, 915, 76, 229, 2440, 0.324, 0.623]]
+    [[229, 880, 864, 380, 575, 763, 305, 457, 76, 114, 229, 1.486, 0.633],
+    [305, 1372, 1344, 610, 845, 915, 610, 915, 76, 229, 305, 1.276, 0.657],
+    [457, 1449, 1420, 762, 1026, 915, 610, 915, 76, 229, 457, 0.966, 0.65],
+    [610, 1525, 1496, 915, 1207, 915, 610, 915, 76, 229, 610, 0.795, 0.64],
+    [915, 1677, 1645, 1220, 1572, 915, 610, 915, 76, 229, 915, 0.608, 0.639],
+    [1220, 1830, 1795, 1525, 1938, 915, 610, 915, 76, 229, 1220, 0.505, 0.634],
+    [1525, 1983, 1941, 1830, 2303, 915, 610, 915, 76, 229, 1525, 0.436, 0.63],
+    [1830, 2135, 2090, 2135, 2667, 915, 610, 915, 76, 229, 1830, 0.389, 0.627],
+    [2135, 2288, 2240, 2440, 3030, 915, 610, 915, 76, 229, 2135, 0.355, 0.625],
+    [2440, 2440, 2392, 2745, 3400, 915, 610, 915, 76, 229, 2440, 0.324, 0.623]]
 """
 class DensidadeViscosidade():
     __slots__=()
@@ -152,8 +170,8 @@ class CP_Methods():
     def fU3(self, Q, C, h3):
         return Q/(C*h3)
         
-    def fL(self, h1, h2):
-        return 6*(h2 - h1)
+    def fLr(self, c, h1, h2):
+        return c*(h2 - h1)
 
     def fh(self, h1, h2):
         return ((h2 - h1)**3)/(4*h1*h2)
@@ -185,8 +203,13 @@ class Níveis():
 
     def setCDKN_kn(self, i):
         self.W = self.dimençõesPadronizadas[i][0]/1000
+        self.A = self.dimençõesPadronizadas[i][1]/1000
+        self.B = self.dimençõesPadronizadas[i][2]/1000
         self.C = self.dimençõesPadronizadas[i][3]/1000
         self.D = self.dimençõesPadronizadas[i][4]/1000
+        self.E = self.dimençõesPadronizadas[i][5]/1000
+        self.F = self.dimençõesPadronizadas[i][6]/1000
+        self.L = self.dimençõesPadronizadas[i][7]/1000
         self.K = self.dimençõesPadronizadas[i][8]/1000
         self.N = self.dimençõesPadronizadas[i][9]/1000
         self.k = self.valoreskn[i][1]
@@ -207,9 +230,9 @@ class Níveis():
         self.h2 = self.fh2(self.h1, self.F1)
         self.h3 = self.fh3(self.h2, self.N, self.K)
         self.U3 = self.fU3(self.Q, self.C, self.h3)
-        self.L = self.fL(self.h1, self.h2)
+        self.Lr = self.fLr(self.c, self.h1, self.h2)
         self.h = self.fh(self.h1, self.h2)
-        self.Tm = self.fTm(self.L, self.U1, self.U3)
+        self.Tm = self.fTm(self.Lr, self.U1, self.U3)
         self.Gm = self.fGm(self.h, self.g, self.rho, self.mu, self.Tm)
 
     def checagem(self):
@@ -230,11 +253,114 @@ class Níveis():
             self.arredondamento()
 
 
-            
+# https://books.google.com.br/books?id=uPSoCwAAQBAJ&pg=PA274&lpg=PA274&dq=save+render_to_string+file+django&source=bl&ots=igy7pOfeOy&sig=ZFgxmOitm8LHSICjQfWy5ZvdtTI&hl=pt-BR&sa=X&ved=0ahUKEwjfoO2FmLPSAhVDG5AKHXwjBrUQ6AEITDAG#v=onepage&q=save%20render_to_string%20file%20django&f=false            
 
-class Main():
+class GerarPDF():
     __slots__=()
 
+    # https://blog.sevenbyte.org/2014/09/23/generating-pdfs-with-django-and-latex.html
+    def gerar_pdf(self):
+        self.dimensionar()
+        context = self.out
+
+        template = get_template('project/calha_parshall2/calha_parshall_latex/mestre.tex')
+        rendered_tpl = template.render(context).encode('utf-8')
+        
+        with tempfile.TemporaryDirectory() as tempdir:
+            #import shutil
+            #path = 'soma/project/templates/project/calha_parshall2/calha_parshall_latex/mestre.pdf'
+            #path = 'project/templates/project/calha_parshall2/calha_parshall.pdf'
+            #shutil.copy(path, tempdir)
+            for i in range(2):
+                process = Popen(
+                    ['pdflatex', '-output-directory', tempdir],
+                    stdin=PIPE,
+                    stdout=PIPE,
+                )
+                process.communicate(rendered_tpl)
+            with open(os.path.join(tempdir, 'texput.pdf'), 'rb') as f:
+                pdf = f.read()
+
+            r = HttpResponse(content_type='application/pdf')  
+            # r['Content-Disposition'] = 'attachment; filename=texput.pdf'
+            r.write(pdf)
+
+        return r     
+
+    def renderizar_template(self, path, template_name, context):    
+        return render_to_string(path + template_name + '.tex', context) 
+
+    def salvar_template_rederizado(self, rendered_templete, path, template_name):
+        with open(path + template_name + '_rendered' + '.tex', 'w') as rt:
+            rendered_templete.encode('utf-8')
+            rt.write(rendered_templete)        
+
+    def criar_pdf(self, path, template_name):
+        initial_path = os.getcwd()
+        os.chdir(path)
+        subprocess.call(['pdflatex', template_name])    
+        os.chdir(initial_path)
+
+    def context_figura(self):
+        d = {
+            'H0':self.H0,
+            'V0':self.U0,
+            'N':self.N,
+            'D':self.D,
+            'D0':self.D0,
+            'W':self.W,
+            'B':self.B,
+            'B23':(2*self.B)/3, # TODO: Criar uma função que calcula (2/3)*B
+            'C':self.C,
+            'F':self.F,
+            'L':self.L,
+            'N':self.N, 
+            'k':self.k,
+            'h1':self.h1,
+            'h2':self.h2,
+            'h3':self.h3,
+            'k':self.k,
+            'L':self.L,
+            'D':self.D,
+        }
+        for key in d:
+            d[key] = round(d[key], 3)
+        return d
+
+    def render_pdf(self, path):
+        with open(path, 'rb') as pdf:
+            response = HttpResponse(pdf.read(),content_type='application/pdf')
+            response['Content-Disposition'] = 'inline; filename=some_file.pdf'
+        return response
+
+    def gerar_figura_calha_parshal(self):
+        self.dimensionar()
+        
+        path1 = 'project/calha_parshall2/calha_parshall_latex/'
+        template_name = 'calha_parshall_builded'
+        context = self.context_figura()
+        rendered_templete = self.renderizar_template(path1, template_name, context)
+
+        try:
+            pathSTR = 'soma/project/templates/' + path1
+            self.salvar_template_rederizado(rendered_templete, pathSTR, template_name)
+            path2 = 'soma/project/templates/project/calha_parshall2/calha_parshall_latex/'
+            template_name = 'calha_parshall_builded_rendered.tex'
+            self.criar_pdf(path2, template_name)
+
+            path2 = path2 + 'calha_parshall_builded_rendered.pdf'
+            return self.render_pdf(path2)
+        except:
+            pathSTR = 'project/templates/' + path1
+            self.salvar_template_rederizado(rendered_templete, pathSTR, template_name)            
+
+            path2 = 'project/templates/project/calha_parshall2/calha_parshall_latex/'
+            template_name = 'calha_parshall_builded_rendered.tex'
+            self.criar_pdf(path2, template_name)
+
+            path2 = path2 + 'calha_parshall_builded_rendered.pdf'
+            return self.render_pdf(path2)
+        
 
 
 
@@ -244,14 +370,12 @@ def factory_CP(cpinit):
     d = dict(cpinit, **cpresults)
 
     class CP(CP_Methods, TabelasCalhaParshall, DensidadeViscosidade, Extras,
-                Níveis):
+                Níveis, GerarPDF):
         __slots__ = [key for key in d]
         def __init__(self):
             for key in d:
                 setattr(self, key, d[key])
     return CP
-
-
 
 
 
