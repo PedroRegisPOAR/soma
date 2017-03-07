ff1c1b6090bcb909cd13d66df60983020839027f
Pedro Regis <pedroalencarregis@hotmail.com>
2017-03-06 21:48:52 -0300

    Organização dos testes em níveis.

--- project/calculos/calha_parshall/tests_calha_parshall.py
+++ project/calculos/calha_parshall/tests_calha_parshall.py
@@ -1,10 +1,9 @@
 ﻿import unittest
 
-from calha_parshall import factory_CP, cpinit, cpresults
+from calha_parshall import factoryCalhaParshall, cpinit, cpresults
 
 # Link interessante
 # http://www.dec.ufcg.edu.br/saneamento/PARSHALL.html
-
 
 
 cpinit_test1 = dict(cpinit)
@@ -13,8 +12,14 @@ cpresults_test1 = dict(cpresults)
 cpresults_test1['mu'] = 0.001065
 cpresults_test1['rho'] = 998.68
 cpresults_test1['W'] = 2.44
+cpresults_test1['A'] = 2.44
+cpresults_test1['B'] = 2.392
+cpresults_test1['B23'] = 1.6
 cpresults_test1['C'] = 2.745
 cpresults_test1['D'] = 3.4
+cpresults_test1['E'] = 0.915
+cpresults_test1['F'] = 0.61
+cpresults_test1['L'] = 0.915
 cpresults_test1['K'] = 0.076
 cpresults_test1['N'] = 0.229
 cpresults_test1['k'] = 0.389
@@ -43,34 +48,34 @@ cpinit_test1['c'] = 6
 cpinit_test1['iW'] = 10
 
 
-def test_CP(args_cp, args_tests):
+def testsNível1(args, args_tests):
     class MyTestCase(unittest.TestCase):
         def setUp(self):
-            CP = factory_CP(args_cp)
+            CP = factoryCalhaParshall(args)
             self.cp = CP()
             self.cp.dimensionar()   
 
-            d = dict(args_cp, **args_tests)
+            d = dict(args, **args_tests)
             for key in d:
                 setattr(self, key, d[key])     
 
         def tearDown(self):
             del self.cp
-
+        
         def test_W(self):
             self.assertTrue( abs(self.cp.W - self.W) < 10**(-1), 
             'A variavel W esta errada. O valor deveria ser W={}, mas é {}'
             .format(self.W, self.cp.W)) 
 
-        def test_mu(self):
-            self.assertTrue( abs(self.cp.mu - self.mu) < 10**(-1), 
-            'A variavel mu esta errada. O valor deveria ser mu={}, mas é {}'
-            .format(self.mu, self.cp.mu)) 
+        def test_A(self):
+            self.assertTrue( abs(self.cp.A - self.A) < 10**(-2), 
+            'A variavel A esta errada. O valor deveria ser A={}, mas é {}'
+            .format(self.A, self.cp.A)) 
 
-        def test_rho(self):
-            self.assertTrue( abs(self.cp.rho - self.rho) < 10**(-2), 
-            'A variavel rho esta errada. O valor deveria ser rho={}, mas é {}'
-            .format(self.rho, self.cp.rho)) 
+        def test_B(self):
+            self.assertTrue( abs(self.cp.B - self.B) < 10**(-2), 
+            'A variavel B esta errada. O valor deveria ser B={}, mas é {}'
+            .format(self.B, self.cp.B))
 
         def test_C(self):
             self.assertTrue( abs(self.cp.C - self.C) < 10**(-1), 
@@ -81,6 +86,21 @@ def test_CP(args_cp, args_tests):
             self.assertTrue( abs(self.cp.D - self.D) < 10**(-1), 
             'A variavel D esta errada. O valor deveria ser D={}, mas é {}'
             .format(self.D, self.cp.D)) 
+
+        def test_E(self):
+            self.assertTrue( abs(self.cp.E - self.E) < 10**(-1), 
+            'A variavel E esta errada. O valor deveria ser E={}, mas é {}'
+            .format(self.E, self.cp.E)) 
+
+        def test_F(self):
+            self.assertTrue( abs(self.cp.F - self.F) < 10**(-1), 
+            'A variavel E esta errada. O valor deveria ser F={}, mas é {}'
+            .format(self.F, self.cp.F)) 
+
+        def test_L(self):
+            self.assertTrue( abs(self.cp.L - self.L) < 10**(-1), 
+            'A variavel L esta errada. O valor deveria ser L={}, mas é {}'
+            .format(self.L, self.cp.L)) 
 
         def test_K(self):
             self.assertTrue( abs(self.cp.K - self.K) < 10**(-1), 
@@ -100,7 +120,17 @@ def test_CP(args_cp, args_tests):
         def test_n(self):
             self.assertTrue( abs(self.cp.n - self.n) < 10**(-1), 
             'A variavel n esta errada. O valor deveria ser n={}, mas é {}'
-            .format(self.n, self.cp.n)) 
+            .format(self.n, self.cp.n))   
+
+        def test_mu(self):
+            self.assertTrue( abs(self.cp.mu - self.mu) < 10**(-1), 
+            'A variavel mu esta errada. O valor deveria ser mu={}, mas é {}'
+            .format(self.mu, self.cp.mu)) 
+
+        def test_rho(self):
+            self.assertTrue( abs(self.cp.rho - self.rho) < 10**(-2), 
+            'A variavel rho esta errada. O valor deveria ser rho={}, mas é {}'
+            .format(self.rho, self.cp.rho)) 
 
         def test_H0(self):
             self.assertTrue( abs(self.cp.H0 - self.H0) < 10**(-1), 
@@ -112,31 +142,116 @@ def test_CP(args_cp, args_tests):
             'A variavel D0 esta errada. O valor deveria ser D0={}, mas é {}'
             .format(self.D0, self.cp.D0)) 
 
+        def test_q(self):
+            self.assertTrue( abs(self.cp.q - self.q) < 10**(-3), 
+            'A variavel q esta errada. O valor deveria ser q={}, mas é {}'
+            .format(self.q, self.cp.q)) 
+
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível2(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
+
         def test_U0(self):
             self.assertTrue( abs(self.cp.U0 - self.U0) < 10**(-3), 
             'A variavel U0 esta errada. O valor deveria ser U0={}, mas é {}'
             .format(self.U0, self.cp.U0)) 
 
-        def test_q(self):
-            self.assertTrue( abs(self.cp.q - self.q) < 10**(-3), 
-            'A variavel q esta errada. O valor deveria ser q={}, mas é {}'
-            .format(self.q, self.cp.q)) 
+        def test_B23(self):
+            self.assertTrue( abs(self.cp.B23 - self.B23) < 10**(-2), 
+            'A variavel B23 esta errada. O valor deveria ser B23={}, mas é {}'
+            .format(self.B23, self.cp.B23)) 
+
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível3(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
 
         def test_E0(self):
             self.assertTrue( abs(self.cp.E0 - self.E0) < 10**(-2), 
             'A variavel E0 esta errada. O valor deveria ser E0={}, mas é {}'
             .format(self.E0, self.cp.E0)) 
 
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível4(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
+
         def test_U1(self):
             self.assertTrue( abs(self.cp.U1 - self.U1) < 10**(-2), 
             'A variavel U1 esta errada. O valor deveria ser U1={}, mas é {}'
-            .format(self.U1, self.cp.U1)) 
+            .format(self.U1, self.cp.U1))
+
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível5(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
 
         def test_h1(self):
             self.assertTrue( abs(self.cp.h1 - self.h1) < 10**(-3), 
             'A variavel h1 esta errada. O valor deveria ser h1={}, mas é {}'
             .format(self.h1, self.cp.h1)) 
 
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível6(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
+        
         def test_F1(self):
             self.assertTrue( abs(self.cp.F1 - self.F1) < 10**(-2), 
             'A variavel F1 esta errada. O valor deveria ser F1={}, mas é {}'
@@ -147,90 +262,115 @@ def test_CP(args_cp, args_tests):
             'A variavel h2 esta errada. O valor deveria ser h2={}, mas é {}'
             .format(self.h2, self.cp.h2)) 
 
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível7(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])      
+        def tearDown(self):
+            del self.cp
+
+        def test_Lr(self):
+            self.assertTrue( abs(self.cp.Lr - self.Lr) < 10**(-2), 
+            'A variavel Lr esta errada. O valor deveria ser Lr={}, mas é {}'
+            .format(self.Lr, self.cp.Lr)) 
+
         def test_h3(self):
             self.assertTrue( abs(self.cp.h3 - self.h3) < 10**(-1), 
             'A variavel h3 esta errada. O valor deveria ser h3={}, mas é {}'
             .format(self.h3, self.cp.h3)) 
+            
+        def test_h(self):
+            self.assertTrue( abs(self.cp.h - self.h) < 10**(-1), 
+            'A variavel h esta errada. O valor deveria ser h={}, mas é {}'
+            .format(self.h, self.cp.h))
+
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível8(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
 
         def test_U3(self):
             self.assertTrue( abs(self.cp.U3 - self.U3) < 10**(-1), 
             'A variavel U3 esta errada. O valor deveria ser U3={}, mas é {}'
             .format(self.U3, self.cp.U3)) 
 
-        def test_L(self):
-            self.assertTrue( abs(self.cp.Lr - self.Lr) < 10**(-2), 
-            'A variavel Lr esta errada. O valor deveria ser Lr={}, mas é {}'
-            .format(self.Lr, self.cp.Lr)) 
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
 
-        def test_h(self):
-            self.assertTrue( abs(self.cp.h - self.h) < 10**(-1), 
-            'A variavel h esta errada. O valor deveria ser h={}, mas é {}'
-            .format(self.h, self.cp.h)) 
+def testsNível9(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
 
         def test_Tm(self):
             self.assertTrue( abs(self.cp.Tm - self.Tm) < 10**(-2), 
             'A variavel Tm esta errada. O valor deveria ser Tm={}, mas é {}'
             .format(self.Tm, self.cp.Tm)) 
 
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+
+def testsNível10(args, args_tests):
+    class MyTestCase(unittest.TestCase):
+        def setUp(self):
+            CP = factoryCalhaParshall(args)
+            self.cp = CP()
+            self.cp.dimensionar()   
+
+            d = dict(args, **args_tests)
+            for key in d:
+                setattr(self, key, d[key])     
+
+        def tearDown(self):
+            del self.cp
+
         def test_Gm(self):
             self.assertTrue( abs(self.cp.Gm - self.Gm) < 10**(1), 
             'A variavel Gm esta errada. O valor deveria ser Gm={}, mas é {}'
-            .format(self.Gm, self.cp.Gm)) 
-    
-    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)            
-        
+            .format(self.Gm, self.cp.Gm))
+
+    return unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
 
 
+n1 = testsNível1(cpinit_test1, cpresults_test1)  
+n2 = testsNível2(cpinit_test1, cpresults_test1)  
+n3 = testsNível3(cpinit_test1, cpresults_test1)  
+n4 = testsNível4(cpinit_test1, cpresults_test1)  
+n5 = testsNível5(cpinit_test1, cpresults_test1)  
+n6 = testsNível6(cpinit_test1, cpresults_test1)  
+n7 = testsNível7(cpinit_test1, cpresults_test1)  
+n8 = testsNível8(cpinit_test1, cpresults_test1)  
+n9 = testsNível9(cpinit_test1, cpresults_test1)  
+n10 = testsNível10(cpinit_test1, cpresults_test1)  
 
-exemploLivro = test_CP(cpinit_test1, cpresults_test1)  
+exemploLivro = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10]
 
-
-#n1 = unittest.TestLoader().loadTestsFromTestCase(exemplo)
-#suite1=[exemploLivro, ]
-suite1=[exemploLivro]
-
-alltests = unittest.TestSuite(suite1)
+alltests = unittest.TestSuite(exemploLivro)
 result = unittest.TextTestRunner(verbosity=2).run(alltests)
         
-
-
-
-def myprint(cp):
-    print("W = ", cp.out['W'])
-    print("C = ", cp.out['C'])
-    print("D = ", cp.out['D'])
-    print("K = ", cp.out['K'])
-    print("N = ", cp.out['N'])
-
-    print("k = ", cp.out['k'])
-    print("n = ", cp.out['n'])
-
-    print("H0 = ", cp.out['H0'])
-    print("D0 = ", cp.out['D0'])
-    print("U0 = ", cp.out['U0'])
-
-    print("q = ", cp.out['q'])
-    print("E0 = ", cp.out['E0'])
-    print("U1 = ", cp.out['U1'])
-    print("h1 = ", cp.out['h1'])
-
-    print("F1 = ", cp.out['F1'])
-
-    print("h2 = ", cp.out['h2'])
-    print("h3 = ", cp.out['h3'])
-    print("U3 = ", cp.out['U3'])
-
-    print("L = ", cp.out['L'])
-    print("h = ", cp.out['h'])
-
-    print("T = ", cp.out['Tm'])
-    print("G = ", cp.out['Gm'])
-
-
-
-
-
-
-
-
-
